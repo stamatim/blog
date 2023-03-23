e476ea2992f5ee6e284f9a5d915a4355af1f93c1
Stamati Morellas <smorellas@protonmail.com>
2023-03-22 20:55:39 -0500

    Fix build error

--- pages/dev.jsx
+++ pages/dev.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import '../styles/tailwind.css'
 
 export default function Dev() {
     return (
         <h1 className="text-3xl font-bold underline">
             Hello world!
         </h1>
-
     )
 }
